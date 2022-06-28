169d55c6475e05468911c6609be252a75e3db2eb
ArlisonMacedo <arlisonmacedo526@gmail.com>
2022-06-28 12:29:55 -0300

    configuração criar posts

---
+++ src/Controller/PostController.js
@@ -0,0 +1,12 @@
+
+
+
+class PostController {
+
+    async create (request, response) {
+        
+    }
+
+}
+
+module.exports = PostController
---
+++ src/database/migrations/01_create_posts.js
@@ -0,0 +1,21 @@
+const knex = require('knex')
+
+exports.up = function (knex) {
+    return knex.schema.createTable('posts', table => {
+        table.increments('id')
+        table.text('text').nullable()
+        table.integer('match').nullable()
+        table.integer('like').notNullable()
+        table.text('comment').nullable()
+        table.text('image').nullable()
+
+        table.integer('user_id')
+            .notNullable()
+            .references('id')
+            .inTable('users')
+    })
+}
+
+exports.down = function () {
+    return knex.schema.dropTable('posts')
+}
